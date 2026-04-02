6d8abe69cb36f7c9ba624261793751335728411f
syedatayyabasource <syedatayyaba2005@gmail.com>
2026-04-02 21:27:00 +0500

    Update test_logic.py

--- tests/test_logic.py
+++ tests/test_logic.py
@@ -2,4 +2,4 @@ import unittest
 
 class TestCareerPath(unittest.TestCase):
     def test_basic(self):
-        self.assertEqual(1, 1) # Ye test hamesha pass hoga
+        self.assertEqual(1, 2) # Ye test hamesha pass hoga
